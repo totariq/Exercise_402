fbee1c2d5c3a00143b235b1ec47d5c6de4df02bf
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-23 19:15:04 -0400

    message

---
+++ src/main/java/com/example/demo/Exercise402Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise402Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise402Application.class, args);
+    }
+
+}
